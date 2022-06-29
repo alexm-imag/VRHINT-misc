9c51744a38034112476afacd6b7e109ad9caae44
Alexander <55482813+alexm-imag@users.noreply.github.com>
2022-06-29 11:50:33 +0200

    Removed deprecated stuff

--- HINT-python/hintFunctions.py
+++ HINT-python/hintFunctions.py
@@ -5,154 +5,18 @@ Created on Fri May 20 13:50:50 2022
 @author: cocot
 """
 import numpy as np;
-#import json;
 import soundfile as sf;
-#import sounddevice as sd;
 import csv;
 import datetime as dt;
 import hintUtilities as util
 
-#%% Global / static
-# variables that are always constant / not to be changed via GUI
-
-# number of sentences in each list
-listSentences = 20;
-# lists open for test (1...10)
-availableTestLists = 10;
-# lists open for practice (11,12)
-availablePracticeLists = 2;
-
-# maybe add these to setup
-ChLeft = 1;
-ChFront = 2;
-ChRight = 2;
-
-
-#%% Setup variables
-hintDir = 'german-hint-adaptive-48kHz\\';  
-    
-
-# Params:
-# testLists: array, determining numTestLists and order
-# testConditions: array, has to match length of testLists!
-# storeResults: bool, create and fill result storage
-# userName: string, required for JSON export
-def hintProcedure(testLists, testConditions, storeResults, sentenceCbk, snrCbk):
-    
-    if storeResults == True:
-        resultStorage = createResultStorage(len(testLists));
-        
-    minSNR = -16;
-    maxSNR = 2;
-    
-    noise, fs = sf.read(hintDir + "noiseGR_male.wav");        
-    
-    for j in range(len(testLists)):
-        sentences = util.loadListSentences(testLists[j], hintDir);
-        randOrder = np.random.permutation(range(20));
-        
-        # store SNR for each sentence
-        listSNR = np.zeros(20);
-        # store hitQuote for each sentence
-        hitQuotes = np.zeros(20);
-        
-        # starting at 0 dB
-        currentSNR = 0;
-    
-    
-        print("Starting List " + str(testLists[j]) + " with condition: " + testConditions[j]);
-    
-        print("List sentences: " + str(listSentences));
-    
-        for i in range(listSentences):
-            # get random index
-            index = randOrder[i];
-            print("Play sentence " + str(index));
-            
-            sentenceCbk(sentences[index]);
-        
-            print(["Current playback level: " + str(currentSNR)]);
-            print(["Round " + str(i) + " out of " + str(listSentences)]);
-            # audio files are labeled from Ger_male001 and not Ger_male000 so add '1'
-            curr_sent = util.loadSentenceAudio(testLists[j], index + 1, currentSNR, hintDir);
-            
-            util.playAudio(curr_sent, noise, testConditions[j], ChFront, ChLeft, ChRight);
-                
-    
-            # get length of current sentence
-            sentenceLength = len(sentences[index].split());
-    
-            # print out current sentence string
-            print("Sentence length: " + str(sentenceLength))
-            print(sentences[index]);    
-    
-            # get experimenter feedback
-            print("How many words have been correct?");
-            #correctWords = input();
-            correctWords = 3;
-        
-            # alternative: just ask if below or above 50% hit
-            # less data but quicker
-            while correctWords > sentenceLength:
-                print("Invalid input. Try again!");
-                print("How many words have been correct?");
-                correctWords = input();
-            
-            print(['Sentence len: ' + str(sentenceLength) + ' correct: ' + str(correctWords)]);
-            hitQuote = correctWords / sentenceLength;
-            print("HitQuote: " + str(hitQuote));
-            
-            # adapt SNR based on hitQuote
-            if i < 4:
-                if hitQuote < 0.5:
-                    currentSNR = currentSNR + 4;
-                else:
-                    currentSNR = currentSNR - 4;
-            else:
-                
-                # store sentence data
-                hitQuotes[i] = hitQuote;
-                listSNR[i] = currentSNR;
-                
-                # adapt SNR based on hitQuote
-                if hitQuote < 0.5:
-                    currentSNR = currentSNR + 2;
-                else:
-                    currentSNR = currentSNR - 2;
-    
-        
-            # SNR sanity check
-            if currentSNR < minSNR:
-                currentSNR = minSNR;
-                print(["Warning: reached min SNR!" + str(minSNR)]);
-            elif currentSNR > maxSNR:
-                currentSNR = maxSNR;
-                print(["Warning: reached max SNR!" + str(maxSNR)]);
-            
-            snrCbk(currentSNR);
-        
-    
-        print(["List" + str(j) + " out of" + str(testLists.count) + " done!"]);
-    
-        if storeResults == True:
-            # store data in resultStorage
-            resultStorage[j]["ListIndex"] = testLists[j];
-            resultStorage[j]["Condition"] = testConditions[j];
-            resultStorage[j]["ListSNRs"] = listSNR;
-            resultStorage[j]["ListHitQuotes"] = hitQuotes;
-            
-            
-    print("Test procedure done!");
-    return resultStorage;
-
-
 class hintTest:
     
     # put stuff here that only has to be set once
-    def __init__(self, testDir, userName, userIndex, numLists = 5, rounds = 20, practiceList = 12, numPracticeSentences = 5, calibrationRounds = 4):
+    def __init__(self, hintDir, userName, numLists = 5, rounds = 20, calibrationRounds = 4, practiceList = 12, numPracticeSentences = 5):
         
-        self.hintDir = testDir;
-        self.userName = userName
+        self.hintDir = hintDir;
+        self.userName = userName;
         
         if numLists > 10:
             print("Error: Invalid list number! Using default instead!");
@@ -191,13 +55,14 @@ class hintTest:
         self.snrStepSize = 2;
         
         self.numTestLists = numLists;
+        self.userIndex = util.getUserIndex();
         
-        [self.testLists, self.testConditions] = self.createTestSetup(userIndex, numLists);
+        [self.testLists, self.testConditions] = self.createTestSetup(self.userIndex, numLists);
         self.resultStorage = self.createResultStorage(numLists, self.sentenceCount, self.calibrationRounds);
         
-        self.noise, self.fs = sf.read(hintDir + "noiseGR_male.wav");    
+        self.noise, self.fs = sf.read(self.hintDir + "noiseGR_male.wav");    
         
-        self.listIndex = 0;#self.testLists[0];
+        self.listIndex = 0;     #self.testLists[0];
         self.sentenceIndex = 0;
         
         self.listSentenceOrder = np.random.permutation(range(self.sentenceCount));
@@ -217,7 +82,7 @@ class hintTest:
         self.chFront = chFront;
         self.chRight = chRight
         
-    def createTestSetup(userIndex, numTestLists):
+    def createTestSetup(self, userIndex, numTestLists):
         
         with open('lqConditions.csv', mode ='r')as file:
           lqConditions = list(csv.reader(file));
@@ -231,7 +96,7 @@ class hintTest:
 
         
         # this will be determined by number of result files!
-        testLists = [lqLists[userIndex % availableTestLists][k] for k in range(numTestLists)];
+        testLists = [lqLists[userIndex % len(lqLists)][k] for k in range(numTestLists)];
         
         # pre-allocate text array
         testConditions = ["emptyCondition" for k in range(numTestLists)]; 
@@ -239,25 +104,29 @@ class hintTest:
         # userIndex determines start line of lqConditions matrix
         lqCondDim = len(lqConditions[0]);
         
-        for i in range(numTestLists / lqCondDim):
+        for i in range((int)(numTestLists / lqCondDim) + 1):
             testConditions[(i * lqCondDim):min(numTestLists, (i *lqCondDim))] = lqConditions[(userIndex + i) % len(lqConditions)];
         
         return testLists, testConditions;
         
-    def createResultStorage(numTestLists, numListSentences, calibRounds):
+    def createResultStorage(self, numTestLists, numListSentences, calibRounds):
         # first 4 rounds of each round are not considered in results
+        print("tempData: " + str(numListSentences - calibRounds));
         templateData = np.zeros(numListSentences - calibRounds);
         templateCondition = "noiseLeft";
         templateListIndex = 12;
         
         resultTemplate = {
             "ListIndex": templateListIndex,
+            "Time": dt.datetime.now().strftime("%d-%m-%y--%H-%M-%S"),
             "Condition": templateCondition,
-            "SNRs": templateData,
-            "HitQuotes:": templateData,
-            "Time": dt.datetime.now().strftime("%d-%m-%y--%H-%M-%S")};
+            "ListSNRs": templateData,
+            "ListHitQuotes:": templateData,
+            "ListSNRAverage": 0
+            };
         
         # allocate numTestLists structs to store results
+        print("NuMTestListCrate:" + str(numTestLists));
         resultStorage = [resultTemplate for k in range(numTestLists)];
         return resultStorage;
         
@@ -269,8 +138,8 @@ class hintTest:
         
         self.listSentenceStrings = util.loadListSentences(self.testLists[self.listIndex], self.hintDir);
         
-        self.listSNR = np.zeros(self.rounds);
-        self.listHitQuotes = np.zeros(self.rounds);
+        self.listSNR = np.zeros(self.sentenceCount  - self.calibrationRounds);
+        self.listHitQuotes = np.zeros(self.sentenceCount - self.calibrationRounds);
         
         self.currSNR = 0;
         self.currCondition = self.testConditions[self.listIndex];
@@ -312,6 +181,8 @@ class hintTest:
     def getCurrentListCount(self):
         return self.listIndex;
     
+    def getUserIndex(self):
+        return self.userIndex;
 
     def practiceSetup(self):
          self.currSNR = 0;
@@ -331,7 +202,7 @@ class hintTest:
         # audio files are labeled from Ger_male001 and not Ger_male000 so add '1'
         currSentenceAudio = util.loadSentenceAudio(self.practiceList, index + 1, self.currSNR, self.hintDir);
             
-        util.playAudio(currSentenceAudio, self.noise, self.practiceCondition, ChFront, ChLeft, ChRight);
+        util.playAudio(currSentenceAudio, self.noise, self.practiceCondition, self.chFront, self.chLeft, self.chRight);
         
     
     def enterPracticeFeedback(self, correctWords):
@@ -375,7 +246,7 @@ class hintTest:
         # audio files are labeled from Ger_male001 and not Ger_male000 so add '1'
         currSentenceAudio = util.loadSentenceAudio(self.currList, index + 1, self.currSNR, self.hintDir);
             
-        util.playAudio(currSentenceAudio, self.noise, self.currCondition, ChFront, ChLeft, ChRight);       
+        util.playAudio(currSentenceAudio, self.noise, self.currCondition, self.chFront, self.chLeft, self.chRight);       
     
    
     def enterFeedback(self, correctWords):    
@@ -448,12 +319,16 @@ class hintTest:
  
     def storeResults(self):
         print(["Storing resutls for list " + str(self.listIndex + 1) + " out of " + str(self.numTestLists)]);
+        print("List index: " + str(self.listIndex));
     
         # store data in resultStorage
         self.resultStorage[self.listIndex]["ListIndex"] = self.testLists[self.listIndex];
         self.resultStorage[self.listIndex]["Condition"] = self.testConditions[self.listIndex];
-        self.resultStorage[self.listIndex]["ListSNRs"] = self.listSNR;
-        self.resultStorage[self.listIndex]["ListHitQuotes"] = self.listHitQuotes;
-        print("List index: " + str(self.listIndex));
-        print(self.resultStorage[self.listIndex]["ListHitQuotes"])
+        self.resultStorage[self.listIndex]["ListSNRs"] = self.listSNR[:];
+        self.resultStorage[self.listIndex]["ListHitQuotes"] = self.listHitQuotes[:];
+        
+        # calculate average SNR of current list
+        self.resultStorage[self.listIndex]["ListSNRAverage"] = sum(self.listSNR) / len(self.listSNR);
+        
+        print(self.resultStorage)
             
