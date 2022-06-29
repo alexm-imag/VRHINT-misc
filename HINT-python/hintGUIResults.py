8b20a2288eaffd8bd78b338590d89e364cba4d70
Alexander <55482813+alexm-imag@users.noreply.github.com>
2022-06-29 13:48:49 +0200

    Got raw version of results screen working

--- HINT-python/hintGUIResults.py
+++ HINT-python/hintGUIResults.py
@@ -31,23 +31,23 @@ class HintResults(ctk.CTkFrame):
         self.userNameLabel = ctk.CTkLabel(self, text="default")
         self.userNameLabel.grid(row = 1, column = 1, pady = 5);
         
-        self.snrLabel = ctk.CTkLabel(self, text="Conditions: ")
-        self.snrLabel.grid(row = 2, column = 0, pady = 5);
+        self.condDescLabel = ctk.CTkLabel(self, text="Conditions: ")
+        self.condDescLabel.grid(row = 2, column = 0, pady = 5);
         
-        self.currentSNR = ctk.CTkLabel(self, text="cond1 cond2")
-        self.currentSNR.grid(row = 2, column = 1, pady = 5);
+        self.condsLabel = ctk.CTkLabel(self, text="cond1 cond2")
+        self.condsLabel.grid(row = 2, column = 1, pady = 5);
         
-        self.snrLabel = ctk.CTkLabel(self, text="Lists: ")
-        self.snrLabel.grid(row = 3, column = 0, pady = 5);
+        self.listsDescLabel = ctk.CTkLabel(self, text="Lists: ")
+        self.listsDescLabel.grid(row = 3, column = 0, pady = 5);
         
-        self.currentSNR = ctk.CTkLabel(self, text="8 4 3")
-        self.currentSNR.grid(row = 3, column = 1, pady = 5);
+        self.listsLabel = ctk.CTkLabel(self, text="8 4 3")
+        self.listsLabel.grid(row = 3, column = 1, pady = 5);
         
-        self.snrLabel = ctk.CTkLabel(self, text="Average SNRs: ")
-        self.snrLabel.grid(row = 4, column = 0, pady = 5);
+        self.avgSNRDescLabel = ctk.CTkLabel(self, text="Average SNRs: ")
+        self.avgSNRDescLabel.grid(row = 4, column = 0, pady = 5);
         
-        self.currentSNR = ctk.CTkLabel(self, text="3.7 -4.9 -2.3")
-        self.currentSNR.grid(row = 4, column = 1, pady = 5);
+        self.avgSNRLabel = ctk.CTkLabel(self, text="3.7 -4.9 -2.3")
+        self.avgSNRLabel.grid(row = 4, column = 1, pady = 5);
         
         
         self.returnBtn = ctk.CTkButton(self, text="Return", padx = 15, pady = 5, bg = "#263D42", command= controller.quit_app)
@@ -58,16 +58,25 @@ class HintResults(ctk.CTkFrame):
         
     
     def setUserName(self, userName):
-        self.userNameLabel['text'] = "TestName";
+        self.userNameLabel['text'] = userName;
+        
+    def setConditions(self, conds):
+        self.condsLabel['text'] = str(conds);
+        
+    def setAverageSNRs(self, snrs):
+            self.avgSNRLabel['text'] = str(snrs);
+        
+    def setLists(self, lists):
+        self.listsLabel['text'] = str(lists);
     
-    def setData(self, hintObject, userName, userIndex):
-        print("Get data from hint Object");
+    def setData(self, hintObject, userName, userIndex):        
         resultData = hintObject.resultStorage;
-        print(resultData);
         self.setUserName(userName);
-        print(resultData['Condition'])
+        conds = [resultData[k]['Condition'] for k in range(len(resultData))]
+        lists = [resultData[k]['ListIndex'] for k in range(len(resultData))]
+        snrs = [resultData[k]['ListSNRAverage'] for k in range(len(resultData))]
         
-        
-    def updateLabels(self):
-        self.pathLabel['text'] = self.path;
+        self.setConditions(conds);
+        self.setLists(lists);
+        self.setAverageSNRs(snrs);
 
